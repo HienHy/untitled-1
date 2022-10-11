23091648e10ab6bb6a1c90a2261cbfeebc46b803
Hiến Nguyễn <hiennguyenhy0911@gmail.com>
2022-10-11 21:03:03 +0700

    !!!

--- src/fraction/Main.java
+++ src/fraction/Main.java
@@ -1,22 +1,22 @@
-package fraction;
+    package fraction;
 
 
-    public class Main {
-        public static void main(String[] args){
-            Fraction p = new Fraction(5,15);
-            Fraction ps = new Fraction(5,6);
+        public class Main {
+            public static void main(String[] args){
+                Fraction p = new Fraction(5,15);
+                Fraction ps = new Fraction(5,6);
 
-            p.printPhanSo();
-            p.rutGon();
-            p.ngichDao();
-            p.printPhanSo();
-            p.add(ps);
-            p.sub(ps);
-            p.div(ps);
-            p.mul(ps);
+                p.printPhanSo();
+                p.rutGon();
+                p.ngichDao();
+                p.printPhanSo();
+                p.add(ps);
+                p.sub(ps);
+                p.div(ps);
+                p.mul(ps);
+            }
+
+
+
         }
-
-
-
-    }
 

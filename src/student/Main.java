8774a583c7803e96eee172f58bcfea41ed99560b
Hiến Nguyễn <hiennguyenhy0911@gmail.com>
2022-10-22 08:07:52 +0700

    up

--- src/student/Main.java
+++ src/student/Main.java
@@ -6,13 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Untitled.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Untitled.fxml")));
         primaryStage.setTitle("Student JavaFX");
         primaryStage.setScene(new Scene(root,800,600));
         primaryStage.show();
